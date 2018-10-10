98d0296af5fd1a20607c37e0e061c67c579cdacf
viraaj83 <44035840+viraaj83@users.noreply.github.com>
2018-10-11 00:08:06 +0530

    half pyramid - c program

---
+++ half pyramid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a;
+    int b;
+	int n;
+    for ( a=1;a<n;a++)
+    {
+        for(b=1;b<a;b++)
+        {
+            printf("%d",b);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
